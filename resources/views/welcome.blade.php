3d3f1fa18675d6a652bca1c5a0f6e8818f954a03
kai <kai.trenbath@gmail.com>
2025-01-03 11:13:11 +0800

    Set up data structure

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+        <div class="max-w-5xl px-4 mx-auto mt-24">
+            <div class="">
+                Hello
+            </div>
+        </div>
+    </body>
+</html>
